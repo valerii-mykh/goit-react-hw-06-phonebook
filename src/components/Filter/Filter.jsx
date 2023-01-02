1b36fd7c3dafe393fcceb45699a17b11acbb8550
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2023-01-02 14:17:13 +0200

    add

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { filterContacts } from 'redux/slices/filter';
+
+export default function Filter() {
+  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter);
+
+  const changeFilter = e => {
+    console.log(e.currentTarget.value);
+    let value = e.currentTarget.value;
+    dispatch(filterContacts(value));
+  };
+
+  return (
+    <label>
+      <input type="text" onChange={changeFilter} />
+    </label>
+  );
+}
