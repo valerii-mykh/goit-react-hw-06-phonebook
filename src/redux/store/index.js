d63190157e154e3c189a482a39518f3327f1dcab
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2023-01-03 22:22:07 +0200

    Update index.js

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -6,7 +6,16 @@
 // export default store;
 
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import contacts from 'redux/slices/contacts';
 import filter from 'redux/slices/filter';
@@ -25,6 +34,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 export const persistor = persistStore(store);
 export default store;
